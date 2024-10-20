0f8be813755143c8fb6c229f3e15f9a173f36be0
Magsmags600 <margaretjenkins@gmail.com>
2024-10-19 21:37:52 -0700

    fixed header links

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Updated for v6
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-import AboutMe from './pages/AboutMe.jsx';
-import Portfolio from './pages/Portfolio.jsx';
-import Contact from './pages/Contact.jsx';
-import Resume from './pages/Resume.jsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 
-const App = () => (
-  <Router>
-    <Header />
-    <main className="container my-5">
+function App() {
+  return (
+    <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<AboutMe />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/resume" element={<Resume />} />
       </Routes>
-    </main>
-    <Footer />
-  </Router>
-);
+    </Router>
+  );
+}
 
 export default App;
